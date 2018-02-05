f86278a84fb196b347bfa939ab75b1506b27e8cb
WorkplaceX <info@workplacex.com>
2018-02-05 18:49:05 +0100

    IsNamingConvention standard

--- Application/Application.cs
+++ Application/Application.cs
@@ -1,7 +1,6 @@
 ﻿namespace Application
 {
     using Framework.Application;
-    using Framework.DataAccessLayer;
     using System;
 
     /// <summary>
@@ -14,45 +13,6 @@
 
     public class AppDemo : App
     {
-        protected override void CellRowValueToText(GridName gridName, Index index, Cell cell, ref string result)
-        {
-            if (index.Enum == IndexEnum.Index)
-            {
-                if (cell.TypeColumn == typeof(bool) || cell.TypeColumn == typeof(bool?))
-                {
-                    if ((bool?)cell.Value == false)
-                    {
-                        result = "No";
-                    }
-                    if ((bool?)cell.Value == true)
-                    {
-                        result = "Yes";
-                    }
-                }
-            }
-        }
-
-        protected override void CellRowValueFromText(GridName gridName, Index index, Cell cell, ref string result)
-        {
-            if (index.Enum == IndexEnum.Index)
-            {
-                if (cell.TypeColumn == typeof(bool) || cell.TypeColumn == typeof(bool?))
-                {
-                    if (result != null)
-                    {
-                        if (result.ToUpper() == "YES")
-                        {
-                            result = "True";
-                        }
-                        if (result.ToUpper() == "NO")
-                        {
-                            result = "False";
-                        }
-                    }
-                }
-            }
-        }
-
         protected override Type TypePageMain()
         {
             return typeof(PageGridDatabaseBrowse);
