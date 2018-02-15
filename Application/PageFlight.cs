4988f91e95d1868e638e423151f01cb296bcefb2
WorkplaceX <info@workplacex.com>
2018-02-15 19:21:18 +0100

    Allow application to run embedded in another web page

--- Application/PageFlight.cs
+++ Application/PageFlight.cs
@@ -4,13 +4,15 @@
     using Framework;
     using Framework.Application;
     using Framework.Component;
+    using Framework.Server;
 
     public class PageFlight : Page
     {
         protected override void InitJson(App app)
         {
             var literalLogo = new Literal(this);
-            literalLogo.TextHtml = "<img class='imgLogo' src='/Arrival.png' />";
+            string url = UtilServer.EmbeddedUrl(app, "/Arrival.png");
+            literalLogo.TextHtml = string.Format("<img class='imgLogo' src='{0}' />", url);
             new GridFieldSingle(this).CssClass = "gridFieldSingle";
             //
             var gridName = Flight.GridName;
