60d576d28bd30eb515ce4d77750d3c1d0ba5054f
WorkplaceX <info@workplacex.com>
2018-12-25 12:26:59 +0100

    Grid cell html

--- Application/App/Airplane.cs
+++ Application/App/Airplane.cs
@@ -23,14 +23,23 @@
             return UtilDal.Query<RawWikipediaAircraft>();
         }
 
-
-
         protected override void GridCellAnnotation(Grid grid, string fieldName, GridRowEnum gridRowEnum, Row row, GridCellAnnotationResult result)
         {
             var aircraft = row as RawWikipediaAircraft;
+
             if (fieldName == nameof(RawWikipediaAircraft.Model) && aircraft?.ModelImageUrl != null)
             {
                 result.HtmlLeft = string.Format("<img src='{0}' width='128px' />", aircraft.ModelImageUrl);
+            }
+
+            if (fieldName == nameof(RawWikipediaAircraft.ModelUrl) && aircraft?.ModelUrl != null)
+            {
+                result.Html = string.Format("<a href='{0}' target='_blank'>{1}", aircraft.ModelUrl, "Wikipedia");
+            }
+
+            if (fieldName == nameof(RawWikipediaAircraft.ModelImageUrl) && aircraft?.ModelImageUrl != null)
+            {
+                result.Html = string.Format("<a href='{0}' target='_blank'>{1}", aircraft.ModelImageUrl, "Image");
             }
         }
     }
