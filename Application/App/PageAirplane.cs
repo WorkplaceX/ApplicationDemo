8fe4108e9b8e52843b40645b3f95b112e308be2d
WorkplaceX <info@workplacex.com>
2019-02-24 21:39:18 +0100

    Config Column Sort

--- Application/App/PageAirplane.cs
+++ Application/App/PageAirplane.cs
@@ -29,8 +29,8 @@
         protected override void GridQueryConfig(Grid grid, ConfigResult result)
         {
             List<FrameworkConfigFieldBuiltIn> list = new List<FrameworkConfigFieldBuiltIn>();
-            list.Add(new FrameworkConfigFieldBuiltIn() { FieldNameCSharp = "IcaoCode", Text = "My IATA", TableNameCSharp = "Demo.RawWikipediaAircraft", IsVisible = true });
-            list.Add(new FrameworkConfigFieldBuiltIn() { FieldNameCSharp = "Model", Text = "My Model", TableNameCSharp = "Demo.RawWikipediaAircraft", IsVisible = true });
+            list.Add(new FrameworkConfigFieldBuiltIn() { FieldNameCSharp = "IcaoCode", Text = "My IATA", TableNameCSharp = "Demo.RawWikipediaAircraft", IsVisible = false });
+            list.Add(new FrameworkConfigFieldBuiltIn() { FieldNameCSharp = "Model", Text = "My Model", TableNameCSharp = "Demo.RawWikipediaAircraft", Sort = 0.5 });
             result.ConfigFieldQuery = list.AsQueryable();
         }
 
