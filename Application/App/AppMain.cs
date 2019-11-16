1b8cd3ba286ca6d7101bd6d84da054043d2db49f
WorkplaceX <info@workplacex.com>
2019-11-16 18:51:32 +0100

    Angular 8 working

--- Application/App/AppMain.cs
+++ Application/App/AppMain.cs
@@ -29,7 +29,7 @@
                 await this.ComponentPageShowAsync<PageDemo>();
             }
 
-            new Html(this).TextHtml = "Build 2019-11-01";
+            new Html(this).TextHtml = "Build 2019-11-16";
         }
 
         private const string sessionExpired = "SessionExpired";
