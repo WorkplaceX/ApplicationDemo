6298289e3ae83ec010eede136ea463f6803da67d
WorkplaceX <info@workplacex.com>
2019-11-18 14:58:01 +0100

    indexUniversal.html

--- Application/App/AppMain.cs
+++ Application/App/AppMain.cs
@@ -29,7 +29,7 @@
                 await this.ComponentPageShowAsync<PageDemo>();
             }
 
-            new Html(this).TextHtml = "Build 2019-11-17";
+            new Html(this).TextHtml = "Build 2019-11-18";
         }
 
         private const string sessionExpired = "SessionExpired";
