607e4191f048a5a924cdd02929523df029183357
WorkplaceX <info@workplacex.com>
2019-12-13 18:24:06 +0100

    .NET Core 3.1

--- Application/App/AppMain.cs
+++ Application/App/AppMain.cs
@@ -31,7 +31,7 @@
 
             this.ComponentCreate<Button>().TextHtml = "Click";
 
-            new Html(this).TextHtml = "<p>Build 2019-12-08 23:16</p>";
+            new Html(this).TextHtml = "<p>Build 2019-12-13 18:22</p>";
         }
 
         private const string sessionExpired = "SessionExpired";
