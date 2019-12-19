a32c123a721cca800e3ed051792bae13ab87127f
WorkplaceX <info@workplacex.com>
2019-12-19 23:13:27 +0100

    Json2 layer deserialize working

--- Application/App/AppMain.cs
+++ Application/App/AppMain.cs
@@ -31,7 +31,7 @@
 
             this.ComponentCreate<Button>().TextHtml = "Click";
 
-            new Html(this).TextHtml = "<p>Build 2019-12-18 15:13</p>";
+            new Html(this).TextHtml = "<p>Build 2019-12-19 23:09</p>";
         }
 
         private const string sessionExpired = "SessionExpired";
