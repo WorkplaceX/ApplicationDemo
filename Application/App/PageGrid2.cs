710c1dee6edbf30cc030cacc51e9b80fd23416ff
WorkplaceX <info@workplacex.com>
2020-01-10 10:52:16 +0100

    Grid handle UOM

--- Application/App/PageGrid2.cs
+++ Application/App/PageGrid2.cs
@@ -19,6 +19,54 @@
             await new Grid2(this).LoadAsync();
         }
 
+        protected override string GridCellText(Grid2 grid, Row row, string fieldName)
+        {
+            if (fieldName == nameof(Navigation.Sort))
+            {
+                return string.Format("{0} {1}", ((Navigation)row).Sort, ((Navigation)row).PageName);
+            }
+            else
+            {
+                return base.GridCellText(grid, row, fieldName);
+            }
+        }
+
+        protected override void GridCellParse(Grid2 grid, Row row, string fieldName, string text, out bool isHandled)
+        {
+            if (fieldName == nameof(Navigation.Sort))
+            {
+                bool isNumber = true;
+                string numberText = null;
+                string measure = numberText;
+                foreach (var item in text)
+                {
+                    if (isNumber)
+                    {
+                        if ((item >= '0' && item <= '9') || item == '.')
+                        {
+                            numberText += item;
+                        }
+                        else
+                        {
+                            isNumber = false;
+                        }
+                    }
+                    if (isNumber == false)
+                    {
+                        measure += item;
+                    }
+                }
+                measure = measure?.Trim();
+                ((Navigation)row).Sort = double.Parse(numberText);
+                ((Navigation)row).PageName = measure;
+                isHandled = true;
+            }
+            else
+            {
+                base.GridCellParse(grid, row, fieldName, text, out isHandled);
+            }
+        }
+
         protected override IQueryable Grid2Query(Grid2 grid)
         {
             return Data.Query<Database.Demo.Navigation>();
