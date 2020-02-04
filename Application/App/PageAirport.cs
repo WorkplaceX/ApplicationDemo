92adb7bb74b4e9e992d5224ac76c59abd481dcd1
WorkplaceX <info@workplacex.com>
2020-02-04 22:20:50 +0100

    BingMap

--- Application/App/PageAirport.cs
+++ Application/App/PageAirport.cs
@@ -14,10 +14,15 @@
         {
             new Html(this) { TextHtml = "<h1>Airport</h1>" };
             Grid = new Grid(this);
+            BingMap = new BingMap(this) { Key = "AhMAiuDtXQ4130XXm2aJD9" 
+                + "MF0KkVTvn3NjY11v9dHBpeuBEkZ96tEGw_Oy9O5w" 
+                + "_I" };
             await Grid.LoadAsync();
         }
 
         public Grid Grid;
+
+        public BingMap BingMap;
 
         protected override IQueryable GridQuery(Grid grid)
         {
@@ -26,6 +31,13 @@
                 return Data.Query<RawOpenFlightsAirport>();
             }
             return base.GridQuery(grid);
+        }
+
+        protected override Task GridRowSelectedAsync(Grid grid)
+        {
+            BingMap.Long = ((RawOpenFlightsAirport)grid.RowSelected).Longitude;
+            BingMap.Lat = ((RawOpenFlightsAirport)grid.RowSelected).Latitude;
+            return base.GridRowSelectedAsync(grid);
         }
     }
 }
