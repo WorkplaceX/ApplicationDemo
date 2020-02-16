27eef87355c45248fbd02e0295c34f21c6a44d86
WorkplaceX <info@workplacex.com>
2020-02-16 19:58:29 +0100

    Add methods Query and RowSelected to generic Grid

--- Application/App/PageAirport.cs
+++ Application/App/PageAirport.cs
@@ -1,10 +1,8 @@
 ﻿namespace Application
 {
     using Database.Demo;
-    using Framework.DataAccessLayer;
     using Framework.Json;
     using Framework.Json.Bing;
-    using System.Linq;
     using System.Threading.Tasks;
 
     public class PageAirport : Page
@@ -15,31 +13,26 @@
         {
             new Html(this) { TextHtml = "<h1>Airport <i class='fas fa-plane-departure'></i></h1>" };
             new Html(this) { TextHtml = "Browse world's airports from <a href='https://openflights.org/data.html' target='_blank'>openflights.org</a> data and see location on Bing maps <a href='https://www.bingmapsportal.com/' target='_blank'>bingmapsportal.com</a>.<br/><br/>" };
-            Grid = new Grid(this);
+            var grid = new GridAirport(this);
             BingMap = new BingMap(this) { Key = "AhMAiuDtXQ4130XXm2aJD9" 
                 + "MF0KkVTvn3NjY11v9dHBpeuBEkZ96tEGw_Oy9O5w" 
                 + "_I" };
-            await Grid.LoadAsync();
+            await grid.LoadAsync();
         }
-
-        public Grid Grid;
 
         public BingMap BingMap;
+    }
 
-        protected override IQueryable GridQuery(Grid grid)
-        {
-            if (grid == Grid)
-            {
-                return Data.Query<RawOpenFlightsAirport>();
-            }
-            return base.GridQuery(grid);
-        }
+    public class GridAirport : Grid<RawOpenFlightsAirport>
+    {
+        public GridAirport(ComponentJson owner) : base(owner) { }
 
-        protected override Task GridRowSelectedAsync(Grid grid)
+        protected override Task RowSelectedAsync()
         {
-            BingMap.Long = ((RawOpenFlightsAirport)grid.RowSelected).Longitude;
-            BingMap.Lat = ((RawOpenFlightsAirport)grid.RowSelected).Latitude;
-            return base.GridRowSelectedAsync(grid);
+            var pageAirport = this.ComponentOwner<PageAirport>();
+            pageAirport.BingMap.Long = RowSelected.Longitude;
+            pageAirport.BingMap.Lat = RowSelected.Latitude;
+            return base.RowSelectedAsync();
         }
     }
 }
