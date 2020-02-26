2a2e7e339ac82d8a39a07b72b0e6951c92b494e5
WorkplaceX <info@workplacex.com>
2020-02-26 14:21:45 +0100

    Move method CellParseFilter(); to Grid

--- Application/App/PageCountry.cs
+++ Application/App/PageCountry.cs
@@ -15,7 +15,7 @@
         {
             new Html(this) { TextHtml = "<h1>Country <i class='fas fa-globe'></i></h1>" };
             new Html(this) { TextHtml = "Browse world's countries from <a href='https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2' target='_blank'>wikipedia.org</a> data. On how to convert and import data to database see <a href='https://github.com/WorkplaceX/Research/tree/master/Wikipedia' target='_blank'>github.com/WorkplaceX/Research</a>. Additionaly data is combined with <a href='https://flagicons.lipis.dev/' target='_blank'>flagicons.lipis.dev</a><br/><br/>" };
-            await new Grid(this).LoadAsync();
+            await new GridCountry(this).LoadAsync();
         }
 
         protected override IQueryable GridQuery(Grid grid)
@@ -61,6 +61,16 @@
             GridCellAnnotationBool(fieldName, row, typeof(CountryDisplayCache), nameof(CountryDisplayCache.IsOpenFlightsAirline), result);
             GridCellAnnotationBool(fieldName, row, typeof(CountryDisplayCache), nameof(CountryDisplayCache.IsOpenFlightsAirport), result);
             GridCellAnnotationBool(fieldName, row, typeof(CountryDisplayCache), nameof(CountryDisplayCache.IsWikipedia), result);
+        }
+    }
+
+    public class GridCountry : Grid<CountryDisplayCache>
+    {
+        public GridCountry(ComponentJson owner) : base(owner) { }
+
+        protected override IQueryable<CountryDisplayCache> Query()
+        {
+            return base.Query().Where(item => item.IsFlagIconCss == true);
         }
     }
 }
