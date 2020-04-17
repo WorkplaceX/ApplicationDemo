2fb7c2cdd7bd76a9cb2cadc4a29e694c091421fa
WorkplaceX <info@workplacex.com>
2020-04-17 17:21:51 +0200

    Update cli

--- Application.Cli/App/AppCliMain.cs
+++ Application.Cli/App/AppCliMain.cs
@@ -33,8 +33,8 @@
             configCli.WebsiteList.Add(new ConfigCliWebsite()
             {
                 DomainNameList = new List<ConfigCliWebsiteDomain>(new ConfigCliWebsiteDomain[] { new ConfigCliWebsiteDomain { EnvironmentName = "DEV", DomainName = "localhost", AppTypeName = appTypeName } }),
-                FolderNameNpmBuild = "Application.Website/Default/",
-                FolderNameDist = "Application.Website/Default/dist/",
+                FolderNameNpmBuild = "Application.Website/MasterDefault/",
+                FolderNameDist = "Application.Website/MasterDefault/dist/",
             });
         }
 
