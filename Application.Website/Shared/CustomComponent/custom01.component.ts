8396aa95e46bf618839894ca17367ead13d8b45e
WorkplaceX <info@workplacex.com>
2020-08-27 17:33:14 +0200

    Animation

--- Application.Website/Shared/CustomComponent/custom01.component.ts
+++ Application.Website/Shared/CustomComponent/custom01.component.ts
@@ -1,22 +1,65 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, NgZone } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: '[data-custom01]',
+  selector: '[data-Custom01]',
   template: `
     <h1>Custom01 Component</h1>
     <p>
       TextHtml=(<span [innerHtml]="json.TextHtml"></span>);
     </p>
+    <p>
+    OffsetMillisecond={{ offsetMillisecond }}; LastUpdate={{ lastUpdate }};
+    </p>
+    <canvas #canvas width="300" height="300"></canvas>
   `,
   styles: [
+    'canvas { border-style: solid }'
   ]
 })
 export class Custom01Component implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private ngZone: NgZone) { }
+
+  offsetMillisecond: number = 0;
+  lastUpdate: string = "";  
+
+  ctx: CanvasRenderingContext2D;
+
+  @ViewChild('canvas', { static: true }) 
+  canvas: ElementRef<HTMLCanvasElement>;
 
   ngOnInit(): void {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    this.ngZone.runOutsideAngular(() => this.tick());
+    setInterval(() =>{
+      this.http.get("http://localhost:5721/", {responseType: 'text'}).subscribe((data) => {
+        let timeResult = <TimeResult>JSON.parse(data);
+        this.offsetMillisecond = timeResult.OffsetMillisecond;
+        this.lastUpdate = timeResult.LastUpdate;
+      });
+    }, 10000);
+  }
+
+  tick() {
+    let timeMillisecond = new Date().getTime();
+    timeMillisecond += this.offsetMillisecond;
+
+    let pixel = ((timeMillisecond % 1000) / 1000) * this.ctx.canvas.height;
+
+    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    this.ctx.fillStyle = 'blue';
+    this.ctx.fillRect(0, 0, this.ctx.canvas.width, pixel);
+
+    window.requestAnimationFrame(() => this.tick());
   }
 
   @Input() json: any;    
+}
+
+class TimeResult
+{
+  OffsetMillisecond: number;
+
+  LastUpdate: string;
 }
--- Application/App/PageAbout.cs
+++ Application/App/PageAbout.cs
@@ -11,6 +11,7 @@
             var container = new BootstrapContainer(this);
             new Html(container) { TextHtml = "<h1>About <i class='fas fa-bullhorn'></i></h1>" };
             new Html(container) { TextHtml = "This is an open source demo application to show WorkplaceX capabilities. It can be downloaded from <a href='https://github.com/WorkplaceX/ApplicationDemo' target='_blank'>github.com/WorkplaceX/ApplicationDemo</a>." };
+            new Custom01(container) { TextHtml = "Hello" };
         }
     }
 }
