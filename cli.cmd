4c8f497dac9c02ba46b6dc1fac35a12901510dae
WorkplaceX <info@workplacex.com>
2020-09-22 17:19:49 +0200

    Update cli command

--- cli.cmd
+++ cli.cmd
@@ -1,6 +1,1 @@
-echo off
-if exist "./Application.Cli/bin/" (
-    dotnet run --project "Application.Cli/Application.Cli.csproj" --no-build -- %*
-) ELSE (
-    dotnet run --project "Application.Cli/Application.Cli.csproj" -- %*
-)
+dotnet run --project "Application.Cli/Application.Cli.csproj" -- %*
